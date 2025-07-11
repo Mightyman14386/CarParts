f8860f3bcc550f2342d7b7a1d0f3d66f21431c86
germilla <87410524+Mightyman14386@users.noreply.github.com>
2025-07-11 16:13:19 -0700

    cleaned up and finished oemparts

--- cars.py
+++ cars.py
@@ -4,14 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
-
-# from IPython.display import display, Image
-
 import time
-# import pandas as pd
-
-# from collections import deque
-# from graphviz import Digraph
 
 options = webdriver.ChromeOptions() 
 options.add_argument('--no-sandbox')
@@ -29,73 +22,44 @@ def car(vin, zip, part):
     print(make, model, modelYear)
 
     # For specific make based sites
-    # match make:
-    #     case "FORD":
-    #         driver.get("https://parts.ford.com")
-    #         time.sleep(0.5)
+    match make:
+        case "FORD":
+            driver.get("https://parts.ford.com")
+            time.sleep(0.5)
 
-    #         driver.find_element(By.XPATH, "//div[@id='tabVin']/button").click()
-    #         time.sleep(0.5)
+            driver.find_element(By.XPATH, "//div[@id='tabVin']/button").click()
+            time.sleep(0.5)
 
-    #         driver.find_element("id","vinNumber_globSearch").send_keys(vin)
-    #         catalog = driver.find_element("id","vinNumButton_globSearch")
-    #         try:
-    #             cookies = driver.find_element(By.XPATH, "//div[@id='onetrust-close-btn-container']/button")
-    #             cookies.click()
-    #         except:
-    #             pass
-    #         catalog.click()
-    #         while True:
-    #             try:
-    #                 driver.find_element("id","noThanksButton").click()
-    #                 driver.find_element("id","autocompleteAddressHomePage").send_keys(zip)
-    #                 break
-    #             except:
-    #                 pass
-    #         driver.find_element("id","cityGoHomePage").click()
-    #         time.sleep(1)
-    #         driver.find_element(By.CSS_SELECTOR, "td.dealerName a.ng-binding").click()
-    #         time.sleep(0.5)
-    #         driver.find_element("id", "SimpleSearchForm_SearchTerm").send_keys(part)
-    #         driver.find_element("id", "searchTermButton").click()
-    #     case "MAZDA":
-    #         pass
-    #     case "KIA":
-    #         pass
-    #     case "MITSUBISHI":
-    #         pass
+            driver.find_element("id","vinNumber_globSearch").send_keys(vin)
+            catalog = driver.find_element("id","vinNumButton_globSearch")
+            try:
+                cookies = driver.find_element(By.XPATH, "//div[@id='onetrust-close-btn-container']/button")
+                cookies.click()
+            except:
+                pass
+            catalog.click()
+            while True:
+                try:
+                    driver.find_element("id","noThanksButton").click()
+                    driver.find_element("id","autocompleteAddressHomePage").send_keys(zip)
+                    break
+                except:
+                    pass
+            driver.find_element("id","cityGoHomePage").click()
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR, "td.dealerName a.ng-binding").click()
+            time.sleep(0.5)
+            driver.find_element("id", "SimpleSearchForm_SearchTerm").send_keys(part)
+            driver.find_element("id", "searchTermButton").click()
+        case "MAZDA":
+            pass
+        case "KIA":
+            pass
+        case "MITSUBISHI":
+            pass
 
     # Try oempartsonline
     try:
-        # driver.get("https://" + make + ".oempartsonline.com")
-        # time.sleep(0.5)
-        # driver.find_element("id","cmsgpf-close-btn").click()
-        # time.sleep(0.5)
-        # driver.find_element(By.CLASS_NAME,"vin_search_str").send_keys(vin)
-        # driver.find_element("id","button-submit").click()
-        # while True:
-        #     try:
-        #         driver.find_element(By.CSS_SELECTOR, "input[type='checkbox']").click()
-        #         break
-        #     except:
-        #         pass
-
-        # driver.get("https://" + make + ".oempartsonline.com/search?search_str=" + vin)
-        # time.sleep(0.5)
-        # driver.find_element("id","cmsgpf-close-btn").click()
-        # time.sleep(0.5)
-        # driver.find_element("id","main_search_8").send_keys(part)
-        # print(driver.get_cookies())
-        #driver.find_element(By.XPATH, "//*[@id='inner-page-search']/div/div/div[2]/div/div/form/fieldset/button").click()
-        
-        # driver.get("https://" + make + ".oempartsonline.com/search?search_str=" + part.replace(" ","+"))
-        # time.sleep(0.5)
-        # driver.find_element("id","cmsgpf-close-btn").click()
-        # time.sleep(0.5)
-        # Select(driver.find_element(By.XPATH,"//*[@id='layout_search']/div/div/div/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/select")).select_by_visible_text(model)
-        # Select(driver.find_element(By.XPATH,"//*[@id='layout_search']/div/div/div/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div/div[3]/select")).select_by_visible_text(modelYear)
-        # driver.get("https://ford.oempartsonline.com/search?search_str=" + part.replace(" ","%20") + "&make=" + make.lower + "&model=" + model.lower + "&year=" + modelYear.lower + "&page_id=&page_url=")
-        
         driver.get("https://" + make + ".oempartsonline.com/search?search_str=" + part.replace(" ","%20") + "&make=" + make.lower() + "&model=" + model.lower() + "&year=" + modelYear + "&page_id=&page_url=")
         time.sleep(0.5)
         driver.find_element("id","cmsgpf-close-btn").click()
@@ -105,18 +69,20 @@ def car(vin, zip, part):
     except Exception as e:
         print(e)
     
+    # Try napaautoparts
     try:
         #driver.get("https://www.napaonline.com/en/search?text=" + part.replace(" ","%20") + "&referer=v2")
         driver.get("https://www.napaonline.com/?srsltid=AfmBOoroip19a6j-FHooIO3pKYXnPXT0Rpi7pAupCEQy_FsdkCiv52Vh")
         time.sleep(0.5)
-        driver.find_element("id","cmsgpf-close-btn").click()
+        #driver.find_element("id","cmsgpf-close-btn").click()
         time.sleep(0.5)
+        driver.find_element("id", "geo-inputText").send_keys(part)
+
     except Exception as e:
         print(e)
 
 
     time.sleep(100)
 
-car("5ux43eu00t9016682", 68521, "wiper blades")
-#1FMCU0MN1RUA03532
+car("1FMCU0MN1RUA03532", 68521, "wiper blades")
 driver.quit()
